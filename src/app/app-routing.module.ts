9836335ee85406a915670ea105240a69fadbcb5e
Allie <amw472@psu.edu>
2022-01-09 22:11:31 -0500

    removing login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,6 @@ import { PicturesComponent } from './components/pictures/pictures.component';
 import { WeddingPartyComponent } from './components/wedding-party/wedding-party.component';
 import { ErrorComponent } from './common/404/404.component';
 import { ScheduleComponent } from './components/schedule/schedule.component';
-import { LoginComponent } from './components/login/login.component';
 import { RegistryComponent } from './components/registry/registry.component';
 import { ContactComponent } from './components/contact/contact.component';
 const routes: Routes = [
@@ -18,7 +17,6 @@ const routes: Routes = [
   { path: 'wedding-party', component: WeddingPartyComponent },
   { path: '404', component: ErrorComponent },
   { path: 'schedule', component: ScheduleComponent },
-  { path: 'login', component: LoginComponent },
   { path: 'registry', component: RegistryComponent },
   { path: 'rsvp', component: ContactComponent } 
 ];
