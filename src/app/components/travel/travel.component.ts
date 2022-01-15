86ff95360fe0a3541f317f50cc11c6d84a1c49bc
Allie <amw472@psu.edu>
2022-01-15 15:59:37 -0500

    adding hotel info to map

--- src/app/components/travel/travel.component.ts
+++ src/app/components/travel/travel.component.ts
@@ -5,20 +5,23 @@ import {
   ElementRef,
   OnInit,
 } from '@angular/core';
+import { MapInfoWindow, MapMarker } from '@angular/google-maps';
 
 @Component({
   selector: 'app-travel',
   templateUrl: './travel.component.html',
   styleUrls: ['./travel.component.scss'],
 })
-export class TravelComponent implements OnInit {
+export class TravelComponent {
   @ViewChild('mapContainer', { static: false }) gmap: ElementRef;
+  @ViewChild(MapInfoWindow, { static: false }) infoWindow: MapInfoWindow;
 
   map: google.maps.Map;
-  foxchaseCoordinates = new google.maps.LatLng(
-    40.21977421038948,
-    -76.13873118849051
+  center = new google.maps.LatLng(
+    40.221268245900376,
+    -76.11751855426823
   );
+  infoContent = '';
 
   markers = [
     {
@@ -29,47 +32,64 @@ export class TravelComponent implements OnInit {
       clickable: true,
       title: 'Foxchase',
       options: {},
+      info: '<a href="https://www.w3schools.com" target="_blank">Visit W3Schools.com!</a>',
     },
     {
       position: {
         lat: 40.22125625561041,
         lng: -76.09662604616365,
       },
+      label: {
+        color: 'black',
+        text: 'A',
+      },
       clickable: true,
       title: 'Tru by Hilton Denver',
       options: {},
+      info: '<a href="https://maps.google.com/maps?ll=40.220914,-76.09695&z=15&t=m&hl=en-US&gl=US&mapclient=apiv3&cid=6531310625964580266" target="_blank"  rel="noopener noreferrer">Tru by Hilton Denver</a>',
     },
     {
       position: {
         lat: 40.22304007938807,
         lng: -76.09825085965382,
       },
+      label: {
+        color: 'black',
+        text: 'B',
+      },
       clickable: true,
       title: 'Comfort Inn Lancaster County North',
       options: {},
+      info: '<a href="https://maps.google.com/maps?ll=40.220964,-76.100033&z=16&t=m&hl=en-US&gl=US&mapclient=apiv3&cid=7371147450018007612" target="_blank"  rel="noopener noreferrer">Comfort Inn Lancaster County North</a>',
     },
     {
       position: {
         lat: 40.22065836368004,
-        lng:  -76.09815114802001,
+        lng: -76.09815114802001,
+      },
+      label: {
+        color: 'black',
+        text: 'C',
       },
       clickable: true,
       title: 'Red Roof Inn Denver',
       options: {},
+      info: '<a href="https://maps.google.com/maps?ll=40.222029,-76.100033&z=16&t=m&hl=en-US&gl=US&mapclient=apiv3&cid=17401549981416588360" target="_blank"  rel="noopener noreferrer">Red Roof Inn Denver</a>',
     },
   ];
 
-  
-  zoom = 13;
-  center = this.foxchaseCoordinates;
+  zoom = 14;
   options: google.maps.MapOptions = {
     zoomControl: true,
-    scrollwheel: true,
+    scrollwheel: false,
     disableDoubleClickZoom: true,
-    maxZoom: 15,
+    maxZoom: 20,
     minZoom: 8,
   };
 
-  ngOnInit() {
+  openInfo(marker: MapMarker, content) {
+    this.infoContent = content;
+    this.infoWindow.open(marker);
+    console.log(marker);
   }
 }
