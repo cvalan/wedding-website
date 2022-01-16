5f5d3dc3275959d595c5a4f69fb9671a3b11ebb9
a7wentz <amwentzel3@gmail.com>
2022-01-16 17:39:21 -0500

    registry updates

--- src/app/components/registry/registry.component.ts
+++ src/app/components/registry/registry.component.ts
@@ -3,6 +3,7 @@ import { Component , OnInit } from '@angular/core';
 @Component({
   selector: 'app-registry',
   templateUrl: './registry.component.html',
+  styleUrls: ['./registry.component.scss'],
 })
 export class RegistryComponent implements OnInit {
   ngOnInit() {
